4a46adc70bda17e951147b81c2019f94ff98fe50
mamun-prodhan <mamunpr9@gmail.com>
2023-09-24 00:43:51 +0600

    outlet and footer added

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./components/Root/Root";
+import Home from "./components/Home/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>,
+    element: <Root></Root>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+    ],
   },
 ]);
 
