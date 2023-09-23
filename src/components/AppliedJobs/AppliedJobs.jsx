adaff4f87a45cdaba38663728ebbf1a376ca122f
mamun-prodhan <mamunpr9@gmail.com>
2023-09-24 01:44:05 +0600

    applied jobs component created

---
+++ src/components/AppliedJobs/AppliedJobs.jsx
@@ -0,0 +1,9 @@
+const AppliedJobs = () => {
+  return (
+    <div>
+      <h2>Jobs I Applied for Career Growth</h2>
+    </div>
+  );
+};
+
+export default AppliedJobs;
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Root from "./components/Root/Root";
 import Home from "./components/Home/Home";
+import AppliedJobs from "./components/AppliedJobs/AppliedJobs";
 
 const router = createBrowserRouter([
   {
@@ -13,6 +14,10 @@ const router = createBrowserRouter([
       {
         path: "/",
         element: <Home></Home>,
+      },
+      {
+        path: "/applied",
+        element: <AppliedJobs></AppliedJobs>,
       },
     ],
   },
