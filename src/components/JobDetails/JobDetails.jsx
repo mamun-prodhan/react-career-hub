5398d7d765b6d6c570d36e04cf9504af1e4e912d
mamun-prodhan <mamunpr9@gmail.com>
2023-09-25 00:46:16 +0600

    toaster added

--- src/components/JobDetails/JobDetails.jsx
+++ src/components/JobDetails/JobDetails.jsx
@@ -1,4 +1,6 @@
 import { useLoaderData, useParams } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const JobDetails = () => {
   const jobsData = useLoaderData();
@@ -7,10 +9,14 @@ const JobDetails = () => {
   const job = jobsData.find((singleJob) => intJobId === singleJob.id);
   console.log(job);
 
+  const handleApplyJob = () => {
+    toast.success("Your have applied successfully");
+  };
+
   return (
     <div className="my-20">
       <h2 className="mb-72 text-3xl font-extrabold text-center">
-        Job Details of {job.id}
+        {job.job_title}
       </h2>
       <div className="grid grid-cols-4 gap-5">
         <div className="col-span-3">
@@ -31,8 +37,41 @@ const JobDetails = () => {
           </p>
           <p>{job.educational_requirements}</p>
         </div>
-        <div></div>
+        {/* job details */}
+        <div>
+          <div className="p-8 bg-[#7E90FE] rounded-lg bg-opacity-10">
+            <p className="py-6 border-b">Job Details</p>
+            <p className="my-6">
+              <span className="font-extrabold">Salary: </span>
+              {job.salary} (Per Month)
+            </p>
+            <p className="mb-6">
+              <span className="font-extrabold">Salary: </span>
+              {job.salary} (Per Month)
+            </p>
+            <p className="font-extrabold pb-6 border-b">Contact Information</p>
+            <p className="my-4">
+              <span className="font-extrabold">Phone: </span>
+              {job.contact_information.phone}
+            </p>
+            <p className="my-4">
+              <span className="font-extrabold">Email: </span>
+              {job.contact_information.email}
+            </p>
+            <p className="my-4">
+              <span className="font-extrabold">Address: </span>
+              {job.contact_information.address}
+            </p>
+          </div>
+          <button
+            onClick={handleApplyJob}
+            className="mt-6 w-full bg-gradient-to-r hover:bg-gradient-to-l duration-300  from-[#7E90FE] to-[#9873FF] px-5 py-3 font-semibold text-[#FFF] rounded-lg"
+          >
+            Apply Now
+          </button>
+        </div>
       </div>
+      <ToastContainer />
     </div>
   );
 };
