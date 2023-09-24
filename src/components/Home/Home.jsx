a40db6a60bc09af3f997cde65c110b4cf0b79a4b
mamun-prodhan <mamunpr9@gmail.com>
2023-09-24 09:42:40 +0600

    components added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,13 @@
-import React from "react";
+import Banner from "../Banner/Banner";
+import FeaturedJobs from "../FeaturedJobs/FeaturedJobs";
+import JobCategory from "../JobCategory/JobCategory";
 
 const Home = () => {
   return (
     <div>
-      <h2>This is Home</h2>
+      <Banner></Banner>
+      <JobCategory></JobCategory>
+      <FeaturedJobs></FeaturedJobs>
     </div>
   );
 };
