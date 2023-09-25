e9dc9c74df5f6ed69665e71f246daef57d933235
mamun-prodhan <mamunpr9@gmail.com>
2023-09-25 13:01:21 +0600

    filter functionality added

--- src/components/AppliedJobs/AppliedJobs.jsx
+++ src/components/AppliedJobs/AppliedJobs.jsx
@@ -1,22 +1,72 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import { getStoredJobApplication } from "../utility/localstorage";
 
 const AppliedJobs = () => {
   const jobs = useLoaderData();
+
+  const [appliedJobs, setAppliedJobs] = useState([]);
+  const [displayJobs, setDisplayJobs] = useState([]);
+
+  const handleJobsFilter = (filter) => {
+    if (filter === "all") {
+      setDisplayJobs(appliedJobs);
+    } else if (filter === "remote") {
+      const remoteJobs = appliedJobs.filter(
+        (jobs) => jobs.remote_or_onsite === "Remote"
+      );
+      setDisplayJobs(remoteJobs);
+    } else if (filter === "onsite") {
+      const onsiteJobs = appliedJobs.filter(
+        (jobs) => jobs.remote_or_onsite === "Onsite"
+      );
+      setDisplayJobs(onsiteJobs);
+    }
+  };
+
   useEffect(() => {
     const storedJobIds = getStoredJobApplication();
-    console.log(storedJobIds);
     if (jobs.length > 0) {
-      const jobsApplied = jobs.filter((job) => storedJobIds.includes(job.id));
-      console.log(jobs, storedJobIds, jobsApplied);
+      // const jobsApplied = jobs.filter((job) => storedJobIds.includes(job.id));
+      const jobsApplied = [];
+      for (const id of storedJobIds) {
+        const job = jobs.find((job) => job.id === id);
+        if (job) {
+          jobsApplied.push(job);
+        }
+      }
+      setAppliedJobs(jobsApplied);
+      setDisplayJobs(jobsApplied);
+      // console.log(jobsApplied);
     }
-  }, []);
+  }, [jobs]);
 
   return (
     <div>
-      <h1>Total Jobs: {jobs.length}</h1>
-      <h2>Jobs I Applied for Career Growth</h2>
+      <h2 className="text-3xl">Jobs I applied {appliedJobs.length}</h2>
+      <details className="dropdown mb-32">
+        <summary className="m-1 btn">open or close</summary>
+        <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
+          <li onClick={() => handleJobsFilter("all")}>
+            <a>All</a>
+          </li>
+          <li onClick={() => handleJobsFilter("remote")}>
+            <a>Remote</a>
+          </li>
+          <li onClick={() => handleJobsFilter("onsite")}>
+            <a>Onsite</a>
+          </li>
+        </ul>
+      </details>
+      <ul>
+        {displayJobs.map((job) => (
+          <li key={job.id}>
+            <span>
+              {job.job_title} {job.company_name} : {job.remote_or_onsite}
+            </span>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
